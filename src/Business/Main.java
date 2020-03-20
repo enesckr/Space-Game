bf872e88d97c04f279d7ab770244f00e9f3f528d
Enes ÇAKİR <enesckr00@gmail.com>
2020-03-20 18:36:05 +0300

    first commit

---
+++ src/Business/Main.java
@@ -0,0 +1,13 @@
+package Business;
+
+import Gui.FrameGui;
+
+        /* Authors: Enes ÇAKİR - Selçuk ÖZDEMİR - Enis Can KARSLIOĞLU */
+
+public class Main {
+
+    public static void main(String[] args) {
+        FrameGui frame = new FrameGui();
+        frame.getFrame();
+    }
+}
